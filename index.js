2df48fa9a32c3143205a702e56c204bcaa9d02f3
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2017-02-13 15:01:17 +0100

    refactored to quoting

--- index.js
+++ index.js
@@ -1,95 +1,81 @@
-var Twitter = require("twitter");
+"use strict";
 
-var client = new Twitter({
+const DELAY = 5*60*1000;
+const TOKENS = ["54", "47", "4"];
+const SPLIT = /\s+/;
+const YOUTUBE_URL = "https://www.youtube.com/watch?v=qjOZtWZ56lc";
+
+const Twitter = require("twitter");
+let client = new Twitter({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
 	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-var DELAY = 5*60*1000;
-var tokens = ["54", "47", "4"];
-var splitRE = /\s+/;
-var recentlyReplied = {};
-
-function onError(error, response) {
-	console.error("!!!", error, response.headers);
-}
-
-function onReplied(err, tweet, response) {
-	if (err) { 
-		onError(err, response);
-	} else {
-		console.log("Successfully sent tweet", tweet.id_str);
-	}
-}
+process.on("unhandledRejection", (reason, p) => {
+    console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+});
 
 function sendReply(tweet) {
-	var users = tweet.entities.user_mentions.map(function(user) {
-		return user.screen_name;
-	});
+	console.log("Quoting %s ('%s')", tweet.id_str, tweet.text);
 
-	var name = tweet.user.screen_name;
-	recentlyReplied[name] = 1;
-	if (users.indexOf(name) == -1) { users.unshift(name); }
-
-	var text = "That's Numberwang! https://www.youtube.com/watch?v=qjOZtWZ56lc";
-	text = users.map(function(user) { return "@" + user; }).join(" ") + " " + text;
-
-	var data = {
-		status: text,
-		in_reply_to_status_id: tweet.id_str
+	let attachment_url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+	let params = {
+		status: `That's Numberwang! ${YOUTUBE_URL} ${attachment_url}`,
+		attachment_url
 	}
-	client.post("statuses/update", data, onReplied);
+	client.post("statuses/update", params).then(
+		tweet => console.log("Successfully sent tweet %s", tweet.id_str),
+		console.error
+	);
+}
+
+function testWord(word) {
+	return (TOKENS.indexOf(word.toLowerCase()) > -1);
+
+function isRegularWord(word) {
+	let ch = word.charAt(0);
+	return (ch != "@" && ch != "#");
 }
 
 function testTweet(tweet) {
 	if (tweet.retweeted_status) { return false; }
-	if (tweet.user.screen_name in recentlyReplied) { return false; }
 
-	var words = tweet.text.split(splitRE);
-	for (var i=0;i<words.length;i++) {
-		var word = words[i];
-		var ch = word.charAt(0);
-		if (ch == "@" || ch == "#") { continue; } // mentions, hashtags are okay
-		if (tokens.indexOf(word.toLowerCase()) > -1) { continue; } // found token
+	let words = tweet.text.split(SPLIT);
+	let regular = words.filter(isRegularWord);
 
-		return false; // anything else => filter out
-	}
-
-	return true;
+	return regular.every(testWord);
 }
 
 function onTweets(tweets) {
-	available = tweets.filter(testTweet);
-	console.log("Search found", tweets.length, "tweets,", available.length, "of them passed the filter");
+	let available = tweets.filter(testTweet);
+	console.log("Search found %s tweets, %s of them passed the filter", tweets.length, available.length);
 	if (available.length) { sendReply(available[0]); }
 }
 
 function search() {
-	var hours = new Date().getHours();
+	let hours = new Date().getHours();
 
 	if (hours >= 6) {
-		client.get("search/tweets", {q:tokens.join(" OR "), result_type:"recent", count:"100"}, function(err, data, response) {
-			if (err) {
-				onError(err, response);
-			} else {
-				onTweets(data.statuses);
-			}
-		});
+		let params = {
+			q: TOKENS.join(" OR "),
+			result_type: "recent",
+			count: 100
+		}
+		client.get("search/tweets", params).then(data => onTweets(data.statuses), console.error);
 	}
 
 	setTimeout(search, DELAY);
 }
 
-client.get("statuses/home_timeline", {count:"100", trim_user:"true", include_entities:"false"}, function(err, data, response) {
-	if (err) {
-		onError(err, response);
-	} else {
-		data.forEach(function(tweet) {
-			recentlyReplied[tweet.in_reply_to_screen_name] = 1;
-		});
-		console.log("Recently replied to", Object.keys(recentlyReplied).length, "users");
-		search();
-	}
+search();
+/*
+let stream = client.stream('statuses/filter', {track: TOKENS.join(",")})
+stream.on("data", tweet => {
+	if (!tweet.text) { return; }
+	if (!testTweet(tweet)) { return; }
+	sendReply(tweet);
 });
+stream.on("error", console.error);
+*/
